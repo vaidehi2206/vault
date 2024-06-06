cb0d1b80d9758d3bc5c52e7a42e6c0da1d5f6b88
vaidehi2206 <vaidehivikramsinh.dabhi01@student.csulb.edu>
2024-06-06 12:05:31 -0700

    "Updated auth pages to use AuthForm component, added dependencies for @hookform/resolvers, @radix-ui/react-label, react-hook-form, and zod"

---
+++ components/CustomInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FormField, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Label } from '@radix-ui/react-label';
+import { Control, Form } from 'react-hook-form';
+import { authFormSchema } from '@/lib/utils';
+
+interface CustomInput {
+    control: Control<z.infer<typeof authFormSchema>>,
+    name: string,
+    label: string,
+    placeholder: string
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field }) => (
+                <div className='form-item'>
+                    <FormLabel className='form-label'>
+                        {label}
+                    </FormLabel>
+                    <div className='flex w-full flex-col '>
+                        <Input 
+                            placeholder={placeholder}
+                            className='input-class'
+                            type='password'
+                            {...field}
+                        />
+                        <FormMessage className='form-message mt-2' />
+                    </div>
+                </div>
+            )}
+        />
+    );
+}
+
+export default CustomInput;
